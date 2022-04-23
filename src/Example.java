42d376339b86f1906e6d01f0f1e8af4fecc362de
Hussen Ghabayen <ghabayenhussej@gmail.com>
2022-04-23 16:20:13 +0300

    update2

--- src/Example.java
+++ src/Example.java
@@ -3,7 +3,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class Example {
-    public Label lable;
+    public Label text;
     public TextField textName;
     public TextField textPassword;
     public Button click;
