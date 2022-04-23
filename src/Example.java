c204752002159c1be32cfb8aea152bd34658b083
Hussen Ghabayen <ghabayenhussej@gmail.com>
2022-04-23 15:06:26 +0300

    Initial commit

---
+++ src/Example.java
@@ -0,0 +1,10 @@
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Example {
+    public Label lable;
+    public TextField textName;
+    public TextField textPassword;
+    public Button click;
+}
---
+++ src/Test.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Test extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+        System.out.println("hussen is here");
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader=new FXMLLoader(getClass().getResource("example.fxml"));
+
+        Pane pane=fxmlLoader.load();
+
+        Scene scene=new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
